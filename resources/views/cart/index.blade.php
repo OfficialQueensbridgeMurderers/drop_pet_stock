720bd13e9e31c9d4b2f01a6e5947330ac68a510a
201458219 <paquettegirouxe@hotmail.com>
2018-04-15 19:58:00 -0400

    Cart est fini

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -7,39 +7,50 @@
 </head>
 <body>
 
-<form method="post" action="cart">
  {{ csrf_field() }}
 <table style="width: 400px">
 	<tr>
 		<td colspan="4">Votre Panier</td>
 	</tr>
 	<tr>
-		<td>user id</td>
+		<td>nom</td>
 		<td>Quantité</td>
 		<td>Prix Unitaire</td>
 		<td>Action</td>
 	</tr>
-
-	@foreach ($cartItems as $item)	
+	
+	@foreach ($cartItems as $item)
 		<tr>
-		<td>{{$item->id_user}}</td>
-		<td>{{$item->quantity}}</td>
+		<td>{{$item->produit->nom}}</td>	
+	    
+		<form method="get" action="{{ url('/') }}/cart/modifier/{{$item->id}}">
+				<td><input type=text size=4 id=hu name=nombre value={{$item->quantity}} </input>
+                <button type="submit">
+                          modifier
+                </button>
+			</form>
+		</td>
 		<td>{{$item->produit->prix}}</td>
 		<td>
-			<input type=button value=Ne_marche_pas>
-			<input type=hidden name=action value=refresh></td>
+			<form method="get" action="{{ url('/') }}/cart/sup/{{$item->id}}">
+                <button type="submit">
+                          supprimer
+                </button>
+			</form>
 		</th>
 	@endforeach
+	<?php
+	$priceTotal=0;	
+	foreach($cartItems as $p){
+	$price = $p->produit->prix * $p->quantity;
+	$priceTotal = $priceTotal + $price;
+	}
+	?>
 
 	      <tr><td colspan=2> </td>
 	      <td colspan=2>
-	      Total :  $
-	      </td></tr>
-	      <tr><td colspan=4>
-	      <input type=submit value=RafraichirTest>
-	      <input type=hidden name=action value=refresh>
+	      Total :  <?php echo $priceTotal ?> $
 	      </td></tr>
 </table>
-</form>
 </body>
 </html>
